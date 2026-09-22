5195cb5d63dfc1f5c2e0c3a436ca09e227946641
Guram K. <Guram@ksipt.ru>
2026-09-22 12:17:06 +0300

    22.09 dva

--- praktika/type.php
+++ praktika/type.php
@@ -2,5 +2,29 @@
 <h2>int - целые числа</h2>
 <?php
 $number = 67;
-echo $number;
+$numero = 0b010101; //двоичное
+echo $numero
+?>
+<h2>float - числа с плавающей точкой</h2>
+<?php
+$a = 1.2;
+$b = 3.4e5; // 34000
+$c = 5e-3; // 0.005
+echo "a = $a, b = $b, c = $c";
+?>
+<h2>bool - логический тип</h2>
+<?php
+$isNumber = true;
+$isNull = false;
+echo "isNumber = $isNumber, isNull = $isNull"
+?>
+<h2>string - cтроки</h2>
+<?php
+$str = 'isanje';
+$str2 = 'PAREJGUNAS';
+echo $str2
+?>
+<h2>null - ничего</h2>
+<?php               
+$nothing = null;
 ?>
